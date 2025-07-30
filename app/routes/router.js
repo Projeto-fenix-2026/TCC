59d0cc967b3864ee2d0197ede8af909db2bdc34c
Gabriel Ribeiro <rm94922@estudante.fieb.edu.br>
2025-07-30 16:57:48 -0300

    first commit

---
+++ app/routes/router.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var router = express.Router();
+const {body, validationResult} = require("express-validator");
+
+router.get("/", function (req, res) {
+  res.render("pages/login",{ "erros": null, "valores": {"email":"","senha":""} ,"retorno":null }); 
+});
+router.post("/login",
+  body("email").isEmail().withMessage("Este email não é válido"),
+  body("senha").isLength({ min: 8 }).withMessage("Senha deve ter pelo menos 8 caracteres!"),
+
+  function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors);
+      return res.render("pages/login", { "erros": errors, "valores":req.body,"retorno":null});
+    }
+
+      return res.render("pages/login", { "erros": null, "valores":req.body,"retorno":req.body});
+  }
+);
+
+module.exports = router;
