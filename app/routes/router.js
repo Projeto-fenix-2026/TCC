31e3ca330e7cf3001ee2adcf4267e4c7afdb855f
Maria Eduarda Novais <rm94588@estudante.fieb.edu.br>
2025-09-19 10:27:59 +0000

    alteração no home para forms

--- app/routes/router.js
+++ app/routes/router.js
@@ -20,8 +20,8 @@ router.post("/login",
   }
 );
 
-router.get("/home", function (req, res) {
-  res.render("pages/home"); 
+router.get("/forms_doacao", function (req, res) {
+  res.render("pages/forms_doacao"); 
 });
 router.get("/ongs", function (req, res) {
   res.render("pages/ongs"); 
